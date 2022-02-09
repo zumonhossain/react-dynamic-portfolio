3fa8216acc335913540d3e28f7467b9efefc2b6b
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-09 22:06:51 +0600

    Axios Allow Cross Origin

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -35,10 +35,7 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
-        }
 
-        return $next($request);
+        return $next($request)->header('Access-Control-Allow-Origin','*');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,31 @@
 <?php
 
-$router->get('/ChartData', 'ChartDataController@onAllSelect');
+$router->get('/ChartData', ['middleware'=>'auth','uses'=>'ChartDataController@onAllSelect']);
 
-$router->get('/ClientReview', 'ClientReviewController@onAllSelect');
+$router->get('/ClientReview', ['middleware'=>'auth','uses'=>'ClientReviewController@onAllSelect']);
 
-$router->post('ContactSend', 'ContactController@onContactSend');
-
-
-$router->get('/CourseHome', 'CourseController@onSelectFour');
-$router->get('/CourseAll', 'CourseController@onSelectAll');
-$router->post('/CourseDetails', 'CourseController@onSelectDetails');
-
-$router->get('/Footer', 'FooterController@onSelect');
-
-$router->get('/Information', 'InformationController@onSelect');
-
-$router->get('/Services', 'ServiceController@onSelect');
+$router->post('ContactSend', ['middleware'=>'auth','uses'=>'ContactController@onContactSend']);
 
 
-$router->get('/Project', 'ProjectController@onSelect3');
-$router->get('/ProjectAll', 'ProjectController@onSelectAll');
-$router->post('/ProjectDetails', 'ProjectController@onSelectDetail');
+$router->get('/CourseHome', ['middleware'=>'auth','uses'=>'CourseController@onSelectFour']);
+$router->get('/CourseAll', ['middleware'=>'auth','uses'=>'CourseController@onSelectAll']);
+$router->post('/CourseDetails', ['middleware'=>'auth','uses'=>'CourseController@onSelectDetails']);
 
-$router->get('/VideoHome', 'HomeEtcController@onSelectVideo');
-$router->get('/TotalProjectClient', 'HomeEtcController@onSelectProjectClient');
-$router->get('/TechDesc', 'HomeEtcController@onSelectTechDesc');
-$router->get('/HomeTopTitle', 'HomeEtcController@onSelectHomeTopTitle');
+$router->get('/Footer', ['middleware'=>'auth','uses'=>'FooterController@onSelect']);
+
+$router->get('/Information', ['middleware'=>'auth','uses'=>'InformationController@onSelect']);
+
+$router->get('/Services', ['middleware'=>'auth','uses'=>'ServiceController@onSelect']);
+
+
+$router->get('/Project', ['middleware'=>'auth','uses'=>'ProjectController@onSelect3']);
+$router->get('/ProjectAll', ['middleware'=>'auth','uses'=>'ProjectController@onSelectAll']);
+$router->post('/ProjectDetails', ['middleware'=>'auth','uses'=>'ProjectController@onSelectDetail']);
+
+$router->get('/VideoHome', ['middleware'=>'auth','uses'=>'HomeEtcController@onSelectVideo']);
+$router->get('/TotalProjectClient', ['middleware'=>'auth','uses'=>'HomeEtcController@onSelectProjectClient']);
+$router->get('/TechDesc', ['middleware'=>'auth','uses'=>'HomeEtcController@onSelectTechDesc']);
+$router->get('/HomeTopTitle', ['middleware'=>'auth','uses'=>'HomeEtcController@onSelectHomeTopTitle']);
 
 
 
